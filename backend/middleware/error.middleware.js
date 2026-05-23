d5c633c34e260b7bbc5ade76174e7d567b2af1ce
Karan Kumar <kk5214662@gmail.com>
2026-05-23 14:14:44 +0530

    implement authentication and role middleware

---
+++ backend/middleware/error.middleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (
+  err,
+  req,
+  res,
+  next
+) => {
+  res.status(500).json({
+    message: err.message,
+  });
+};
+
+module.exports = errorHandler;
---
+++ backend/middleware/role.middleware.js
@@ -0,0 +1,15 @@
+const adminOnly = (
+  req,
+  res,
+  next
+) => {
+  if (req.user.role !== "Admin") {
+    return res.status(403).json({
+      message: "Admin only access",
+    });
+  }
+
+  next();
+};
+
+module.exports = adminOnly;
